0c7acf9ea2a7fa45547b588491ed07b4aaf5bed1
shemloft <shem.dar99@gmail.com>
2020-02-29 15:38:51 +0500

    player added

--- src/ClientApp/components/App/index.jsx
+++ src/ClientApp/components/App/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './styles.css';
 import Field from '../Field';
 
+const TABLE_WIDTH = 600;
+const cellsInRowAmount = 8
+const cellSize = TABLE_WIDTH / cellsInRowAmount;
+
 export default class App extends React.Component {
     constructor () {
         super();
@@ -16,8 +20,29 @@ export default class App extends React.Component {
                 <div className={ styles.score }>
                     Ваш счет: { this.state.score }
                 </div>
-                <Field />
+                <Field cellSize={cellSize}/>
             </div>
         );
     }
 }
+
+window.addEventListener("keydown", e => {
+    const player = document.getElementById('player');
+    if (e.keyCode >= 37 && e.keyCode <= 40) {
+        let oldx = parseInt(player.style.left.slice(0, -2));
+        let oldy = parseInt(player.style.top.slice(0, -2));
+    
+        if (e.keyCode === 37) {
+            player.style.left = (oldx - cellSize) + 'px';
+        }
+        if (e.keyCode === 39) {
+            player.style.left = (oldx + cellSize) + 'px';
+        }
+        if (e.keyCode === 38) {
+            player.style.top = (oldy - cellSize) + 'px';
+        }
+        if (e.keyCode === 40) {
+            player.style.top = (oldy + cellSize) + 'px';
+        }
+    }
+});
--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './styles.css'
 
-
 class Row extends React.Component {
     constructor(props) {
         super(props);
@@ -43,16 +42,62 @@ class Map extends React.Component {
                 </tbody>
             </table>);
     }
+
+    componentDidMount() {
+
+
+    }
+}
+
+class Player extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            top: 2,
+            left: 3
+        };
+        this.cellSize = props.cellSize;
+    }
+
+    render() {
+        debugger;
+        const style = {
+            width: this.cellSize + 'px',
+            height: this.cellSize + 'px',
+            top: this.state.top * this.cellSize + 'px',
+            left: this.state.left * this.cellSize + 'px',
+            position: 'absolute'
+        }
+        return (
+            <div className={styles.player} style={style} id="player"></div>);
+    }
 }
 
 
+function parseMap(jsonMap) {
+    const values = {
+        "W": styles.wall,
+        "E": styles.empty,
+        "P": styles.player,
+        "T": styles.target,
+        "B": styles.box
+    }
+
+
+}
 
 export default class Field extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cellSize = props.cellSize;
+    }
+
     render() {
         return (
             <div className={styles.center}>
                 <div className={styles.fieldWrapper}>
                     <Map />
+                    <Player cellSize={this.cellSize} />
                 </div>
             </div>
         );
