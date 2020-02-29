c414921192745169d92b8f522921fcb8073aea63
shemloft <shem.dar99@gmail.com>
2020-02-29 14:33:07 +0500

    some front

--- src/ClientApp/components/Field/index.jsx
+++ src/ClientApp/components/Field/index.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
 import styles from './styles.css'
 
-export default class Field extends React.Component {
-    render () {
+
+class Row extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props
+    }
+
+    render() {
+        let cells = []
+        for (let cell of this.props.cells) {
+            if (cell === '')
+                cells.push(<td></td>)
+            else
+                cells.push(<td className={cell}></td>)
+
+        }
+        console.log(cells)
         return (
-            <div className={ styles.root }/>
+            <tr>
+                {cells}
+            </tr>
         );
     }
+}
+
+class Map extends React.Component {
+    render() {
+        return (
+            <table className="field">
+                <tbody>
+                    <Row cells={[styles.empty, styles.empty, styles.wall, styles.wall, styles.wall, styles.wall, styles.wall, styles.empty]} />
+                    <Row cells={[styles.wall, styles.wall, styles.wall, styles.empty, styles.empty, styles.empty, styles.wall, styles.empty]} />
+                    <Row cells={[styles.wall, styles.target, styles.empty, styles.empty, styles.empty, styles.empty, styles.wall, styles.empty]}/>
+                    <Row cells={[styles.wall, styles.wall, styles.wall, styles.empty, styles.box, styles.target, styles.wall, styles.empty]}/>
+                    <Row cells={[styles.wall, styles.target, styles.wall, styles.wall, styles.box, styles.empty, styles.wall, styles.empty]}/>
+                    <Row cells={[styles.wall, styles.empty, styles.wall, styles.empty, styles.target, styles.empty, styles.wall, styles.wall]}/>
+                    <Row cells={[styles.wall, styles.empty, styles.empty, styles.empty, styles.empty, styles.empty, styles.target, styles.wall]}/>
+                    <Row cells={[styles.wall, styles.empty, styles.empty, styles.empty, styles.target, styles.empty, styles.empty, styles.wall]}/>
+                    <Row cells={[styles.wall, styles.wall, styles.wall, styles.wall, styles.wall, styles.wall, styles.wall, styles.wall]}/>
+                </tbody>
+            </table>);
+    }
+}
+
+
+
+export default class Field extends React.Component {
+    render() {
+        return (
+            <div className={styles.center}>
+                <div className={styles.fieldWrapper}>
+                    <Map />
+                </div>
+            </div>
+        );
+    }
+
+    //     <div className={styles.fieldWrapper}>
+    //     <table className={styles.field}>
+    //         <tbody>
+    //             <tr>
+    //                 <td className={styles.wall}>a</td>
+    //             </tr>
+    //         </tbody>
+    //     </table>
+    // </div>
 }
