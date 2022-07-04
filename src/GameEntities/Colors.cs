cf3b91feeab7cb96460b01fccb3f245cb99bce14
Konstantin Luzgin <konstantin.luzgin59@gmail.com>
2022-07-04 11:33:21 +0500

    Добавил Colors enum c 16-ричными кодами цветов, GenerateMap в Game.cs для генерации поля рандомных цветов

---
+++ src/GameEntities/Colors.cs
@@ -0,0 +1,15 @@
+﻿namespace thegame.GameEntities;
+
+public enum Colors
+{
+    Red = 0xFF0000,
+    Orange = 0xFF8700,
+    LightOrange = 0xFFD300,
+    Lime = 0xDEFF0A,
+    Green = 0xA1FF0A,
+    LightGreen = 0x0AFF99,
+    LightBlue = 0x0AEFFF,
+    Blue = 0x147DF5,
+    DeepBlue = 0x580AFF,
+    Magenta = 0xBE0AFF
+}
---
+++ src/GameEntities/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using thegame.Models;
+
+namespace thegame.GameEntities;
+
+public class Game
+{
+    public GameDto dto;
+
+    public Game()
+    {
+        
+    }
+
+    private CellDto[] GenerateMap(int difficulty)
+    {
+        var random = new Random();
+        var size = difficulty + 5;
+
+        var grid = 
+            from x in Enumerable.Range(1, size)
+            from y in Enumerable.Range(1, size)
+            select new CellDto((x * size + y).ToString(),
+                new VectorDto { X = x, Y = y }, 
+                random.Next(1, Enum.GetNames(typeof(Colors)).Length).ToString(),
+                "",
+                0);
+    }
+}
