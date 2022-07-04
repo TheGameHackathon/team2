82e9a24dea1c23f133bf054211eca71a74d24bb0
RufusBarma <neymanyu@gmail.com>
2022-07-04 12:23:18 +0500

    Add MovesOneStepByAI api method

--- src/Controllers/MovesController.cs
+++ src/Controllers/MovesController.cs
@@ -6,15 +6,24 @@ using thegame.Services;
 
 namespace thegame.Controllers;
 
-[Route("api/games/{gameId}/moves")]
+[Route("api/games/{gameId}/[controller]/[action]")]
 public class MovesController : Controller
 {
     [HttpPost]
     public IActionResult Moves(Guid gameId, [FromBody]UserInputDto userInput)
     {
+        if (userInput.KeyPressed == 'i')
+            return MovesOneStepByAI(gameId);
         var game = TestData.AGameDto(userInput.ClickedPos ?? new VectorDto {X = 1, Y = 0});
         if (userInput.ClickedPos != null)
             game.Cells.First(c => c.Type == "#ffffff").Pos = userInput.ClickedPos;
         return Ok(game);
+    }
+    
+    [HttpPost]
+    public IActionResult MovesOneStepByAI(Guid gameId)
+    {
+        throw new NotImplementedException();
+        // return Ok(game);
     }
 }
